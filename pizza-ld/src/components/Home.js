2b2fc4f873eac16d7ea5c788bd93d03ee41e6aae
LdKhanh <khanhleemtp>
2020-08-08 01:34:11 +0700

    3-animating-ele-done

--- pizza-ld/src/components/Home.js
+++ pizza-ld/src/components/Home.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 function Home() {
     return (
-        <div className="home container">
-            <h2>
-                Welcome to pizza LD
-            </h2>
+        <motion.div className="home container"
+            animate={{}}
+        >
+            <h2>Welcome to pizza LD</h2>
             <Link to="/base">
-            <button>
+            <motion.button
+              animate={{}}
+            >
                 Create Your Pizza
-            </button>
+            </motion.button>
             </Link>
-        </div>
+        </motion.div>
     )
 }
 
