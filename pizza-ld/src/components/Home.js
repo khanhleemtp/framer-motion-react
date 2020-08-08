4fe30cb655c9093ed65bb99346305b10266acd43
LdKhanh <khanhleemtp>
2020-08-09 01:38:12 +0700

    16-loader

--- pizza-ld/src/components/Home.js
+++ pizza-ld/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import Loader from './Loader';
 
 const buttonVariants = {
     hover: {
@@ -47,6 +48,7 @@ function Home() {
             >
                 Create Your Pizza
             </motion.button>
+            <Loader />
             </Link>
         </motion.div>
     )
